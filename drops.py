e83c829f4cd547298a0236463b3a614abc6c4bc1
Peter Hurford <peter@peterhurford.com>
2018-12-22 22:26:06 -0500

    Fix drops verbosity

--- drops.py
+++ drops.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
-from cache import load_cache, save_in_cache
+from cache import load_cache, save_in_cache, is_in_cache
 
-def get_drops():
+def get_drops(verbose=True):
     if is_in_cache('drops'):
         drops, _ = load_cache('drops')
         drops = list(drops['drops'].values)
     else:
         drops = []
-    print('There are {} drops'.format(len(drops)))
+    if verbose:
+        print('There are {} drops'.format(len(drops)))
     return drops
 
 def save_drops(drops):
-	save_in_cache('drops', pd.DataFrame({'drops': list(set(drops))}), None)
+    save_in_cache('drops', pd.DataFrame({'drops': list(set(drops))}), None)
 
-def add_drops(drops):
-	old_drops = get_drops()
-	save_drops(drops + old_drops)
+def add_drops(drops, verbose=True):
+    old_drops = get_drops(verbose=False)
+    drops = drops + old_drops
+    save_drops(drops)
+    print('There are {} drops'.format(len(drops)))
+    return drops
